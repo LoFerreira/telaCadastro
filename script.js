6e3a367684cff9b7f71886043fac1684707c2818
Leonardo Ferreira <leonardo10sp@gmail.com>
2021-06-21 11:59:46 -0300

    validação de e-mail

--- script.js
+++ script.js
@@ -40,6 +40,15 @@ let validation = {
                                 return 'Campo tem que ter pelo menos '+rDetails[1]+' caracteres';
                             }
                         break;
+
+                    case 'email':
+                            if(input.value != '') {
+                                let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                                if(!regex.test(input.value.toLowerCase())) {
+                                    return 'E-mail digitado não é válido!'
+                                }
+                            }
+                        break;
                 }
             }
         }
